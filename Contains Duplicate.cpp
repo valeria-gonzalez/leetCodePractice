c7da6fd364ed8677b68aba8e96c34b2e6596bc50
Valeria González <71797910+valeria-gonzalez@users.noreply.github.com>
2023-06-20 22:05:35 -0600

    Update Contains Duplicate problem desc.cpp

--- Contains Duplicate.cpp
+++ Contains Duplicate.cpp
@@ -1,3 +1,10 @@
+/*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+*/
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
