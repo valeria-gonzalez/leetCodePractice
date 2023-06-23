56e02b7521c58a75851ee1ef4334d29e053fb329
Valeria González <71797910+valeria-gonzalez@users.noreply.github.com>
2023-06-23 00:16:56 -0600

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,3 +1,15 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+
+// this solution was all too much 
 class Solution {
 public:
     bool bsearch(int startpos, int search, int n, vector<int>& nums){
@@ -43,5 +55,26 @@ public:
         
         return ans;
         
+    }
+};
+
+// best
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        vector<int> res(2);
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) {
+            if(mp.find(target - nums[i]) != mp.end() && mp[target - nums[i]] != i) {
+                res[0] = i;
+                res[1] = mp[target - nums[i]];
+                return res;
+            }
+            mp[nums[i]] = i; // interesting concept, one can fill a map as they go
+        }
+
+        return res;
     }
 };
